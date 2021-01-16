c8a291ae46a8b026a2bfa35df21f322249c1a24b
davi-java <david96711972@gmail.com>
2021-01-16 17:46:57 -0400

    style class Filmes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   Text,
   StyleSheet,
   FlatList,
+  ActivityIndicator
 } from 'react-native';
 
 import api from './src/Services/api';
@@ -14,28 +15,41 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      filmes: []
+      filmes: [],
+      loading: true
     }
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     const response = await api.get('r-api/?api=filmes');
     this.setState({
-      filmes: response.data
+      filmes: response.data,
+      loading: false
     })
   }
-
-
+  
   render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-        data={this.state.filmes}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => <Filmes data={item}/>}
-        />
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <View style={{ justifyContent: 'center', alignItems:'center', flex:1 }}>
+          <ActivityIndicator
+            color="#09A6FF"
+            size={50}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.filmes}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => <Filmes data={item} />}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      );
+    }
   }
 }
 
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,24 +1,79 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    TouchableOpacity
 } from 'react-native';
 
-export default class Filmes extends Component{
-    render(){
-        return(
+export default class Filmes extends Component {
+    render() {
+        const { nome, foto } = this.props.data;
+        return (
             <View style={styles.container}>
-                <Text>
-                    {this.props.data.nome}
-                </Text>
+
+                <View style={styles.card}>
+                    <Text style={styles.title}>
+                        {nome}
+                    </Text>
+                    <Image
+                        source={{ uri: foto }}
+                        style={styles.capa}
+                    />
+                    <View style={styles.areaBtn}>
+                        <TouchableOpacity 
+                        style={styles.btn}
+                        onPress={() => alert('Filme: ' + nome)} >
+                            <Text style={styles.textBtn}>
+                                LEIA MAIS...
+                        </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1
+    },
+    card:{
+        shadowColor:'#000',
+        backgroundColor:'#FFF',
+        shadowOffset: {width:0,height:1},
+        shadowOpacity: 0.8,
+        margin: 15,
+        shadowRadius: 5,
+        borderRadius: 5,
+        elevation: 3
+    },
+    title:{
+        fontSize: 18,
+        padding: 15
+    },
+    capa:{
+        height:250,
+        zIndex: 2
+    },
+    areaBtn:{
+        alignItems:'flex-end',
+        marginTop: -40,
+        zIndex: 9
+    },
+    btn:{
+        width: 100,
+        backgroundColor:'#09A6FF',
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius:5
+    },
+    textBtn:{
+        textAlign:'center',
+        color:'#FFF'
     }
 })
